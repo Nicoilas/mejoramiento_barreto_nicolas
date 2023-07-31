55d71f59df14e51d16b8862f9b590b2653c72907
Nicolas <nb199144@gmail.com>
2023-07-31 10:59:00 -0500

    correccion

--- ciclos/for3.py
+++ ciclos/for3.py
@@ -1,5 +1,15 @@
-def imprimir_inverso(cadena):
-    for char in reversed(cadena):
-        print(char)
-
-imprimir_inverso("Hola mundo")    
+def encontrar_divisores(numero):
+    divisores = []
+    for i in range(1, numero + 1):
+        if numero % i == 0:
+            divisores.append(i)
+    return divisores
+try:
+    numero = int(input("Ingrese un número entero: "))
+    if numero <= 0:
+        print("Por favor, ingrese un número entero positivo.")
+    else:
+        resultado = encontrar_divisores(numero)
+        print(f"Los divisores de {numero} son: {resultado}")
+except ValueError:
+    print("Por favor, ingrese un número entero válido.")
--- condicionales/5.py
+++ condicionales/5.py
@@ -1,14 +1,23 @@
-def es_bisiesto(anio):
-    if anio % 4 == 0:
-        if anio % 100 == 0:
-            if anio % 400 == 0:
-                return True
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
+def encontrar_valor_medio(num1, num2, num3):
+   
+    lista_numeros = [num1, num2, num3]
 
-print(es_bisiesto(2020))    
-print(es_bisiesto(2021))    
+    if lista_numeros[0] > lista_numeros[1]:
+        lista_numeros[0], lista_numeros[1] = lista_numeros[1], lista_numeros[0]
+    if lista_numeros[0] > lista_numeros[2]:
+        lista_numeros[0], lista_numeros[2] = lista_numeros[2], lista_numeros[0]
+
+    if lista_numeros[1] > lista_numeros[2]:
+        lista_numeros[1], lista_numeros[2] = lista_numeros[2], lista_numeros[1]
+
+    
+    valor_medio = lista_numeros[1]
+
+    return valor_medio
+
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+num3 = int(input("Ingrese el tercer número: "))
+
+valor_medio = encontrar_valor_medio(num1, num2, num3)
+print(f"El valor del medio es: {valor_medio}")
